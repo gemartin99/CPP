c905bd8dc308f181702203134a6c720b8233d295
gemartin99 <gerardmg19953@gmail.com>
2022-11-15 00:20:32 +0100

    update all files

--- CPP00/ex01/main.cpp
+++ CPP00/ex01/main.cpp
@@ -1,9 +1,15 @@
 #include <iostream>
 #include <string>
 
-void ft_add()
+static bool is_digits(const std::string &str)
+{
+	return (str.find_first_not_of("0123456789") == std::string::npos);
+}
+
+void ft_add(Phonebook *p_list)
 {
 	int i;
+	std::string info[5];
 
 	i = -1;
 	while (++i < 5)
@@ -25,15 +31,28 @@ void ft_add()
 			case 4:
 				std::cout << "Introduce the darkest secret" << std::endl;
 				break;
-
-
+		}
+		if (i == 3 && is_digits(info[i]) && vars[i] != "")
+			;
+		else if (!is_digits(info[i]) && vars[i] != "")
+		{
+			std::cout << "Invalid input" << std::endl;
+			i--;
 		}
 	}
+	Contact newcontact(info[0], info[1], info[2], info[3], info[4]);
+	(*p_list).addcontact(newc);
 }
 
 void ft_search()
 {
-
+	if (arrayvacia)
+	{
+		std::cout << "Phonebook is empty. You must add Contacts to be able to search for them." << std::endl;
+		return 0;
+	}
+	std::string	input = "";
+	while ()
 }
 
 int main(int argc, char **argv)
---
+++ CPP00/ex01/phonebook.cpp
@@ -0,0 +1,25 @@
+# include "Phonebook.hpp"
+
+Phonebook::Phonebook(void)
+{
+	len = 0;
+	return;
+}
+
+Phonebook::~Phonebook(void){ return; }
+
+void Phonebook::addcontact(Contact newcontact)
+{
+	if (len < 8)
+	{
+		list[len] = newcontact;
+		list[len].setid(len + 1);
+		len++;
+	}
+	//else
+	//yavere
+}
+
+Contact *Phonebook::getList(void){ return (list); }
+
+int	Phonebook::getlen(void){ return (len); }
